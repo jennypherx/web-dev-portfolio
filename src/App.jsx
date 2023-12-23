30216de927097e05649f4024d30de42b6baa6f26
Jennypher <jennypherx25@gmail.com>
2023-12-23 14:38:36 -0500

    Added the  Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <div>
+      <Header />
+      
       Hello from my web dev portfolio app!
     </div>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    
+    return (
+      <header>
+        
+        <h1>Jennypher Pepin</h1>
+       
+        {/* Links?? */}
+
+      </header>
+    );
+  }
+  
+  export default Header;
