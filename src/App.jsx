90f15396c4c32d1e672806943d92a43a103b090d
Jennypher <jennypherx25@gmail.com>
2023-12-23 14:33:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      Hello from my web dev portfolio app!
+    </div>
+  )
+}
+
+export default App
